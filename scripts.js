4f63dc19461066090891cb4a8e4edce7fe8792aa
davilanza <lanzamatos@gmail.com>
2026-08-12 14:46:02 -0300

    uploading website filles

---
+++ scripts.js
@@ -0,0 +1,9 @@
+mensagem = document.querySelector('#botao')
+mensagem.addEventListener('click', function() {
+    alert('Tenha um ótimo dia!');
+});
+
+let alterarCor = document.querySelector('#cor')
+alterarCor.addEventListener('click', function() {
+    document.body.style.backgroundColor = 'blue';
+});
