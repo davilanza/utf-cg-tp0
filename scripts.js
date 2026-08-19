7182c1067441f668c5bff0f67407d20f1d0a42b7
davilanza <lanzamatos@gmail.com>
2026-08-19 14:32:50 -0300

    adicao de carrossel e fotos, mudanca do background e tamanho do html

--- scripts.js
+++ scripts.js
@@ -1,10 +1,29 @@
 mensagem = document.querySelector('#botao')
+
 mensagem.addEventListener('click', function() {
     alert('Tenha um ótimo dia!');
 });
 
-let alterarCor = document.querySelector('#cor')
-alterarCor.addEventListener('click', function() {
-    document.body.style.backgroundColor = 'rgb(45, 32, 216)';
+let alterarFundo = document.querySelector('#cor')
+
+alterarFundo.addEventListener('click', function() {
+    document.body.classList.toggle('cruzeiro');
 });
 
+let i = 0;
+function mudarFoto(direcao)
+{
+    const imagens = document.getElementById('img_car');
+    const totalImagens = imagens.children.length;
+    const larguraCarrossel = imagens.parentElement.clientWidth;
+    
+    i = i + direcao;
+
+    if (i >= totalImagens) {
+        i = 0;
+      } else if (i < 0) {
+        i = totalImagens - 1;
+    }
+
+    imagens.style.transform = `translateX(${-i * larguraCarrossel}px)`;
+}
